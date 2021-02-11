83e96846acf086bb04ba21c58c7cc2f23cadc0af
Jim <jamestprime02@icloud.com>
2021-02-12 00:08:03 +0200

    Add files via upload

---
+++ src/CommentDetail.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface Props {
+    author: string;
+    date: string;
+    profile: any;
+    text: string;
+}
+const CommentDetail = (props: Props) => {
+    return (
+        <div className="comment">
+            <a href="/" className= "avatar">
+            <img alt="avatar" src={props.profile} />
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    {props.author}
+                </a>
+                <div className="metadata">
+                     <span className="date">{props.date}</span>
+                </div>
+                <div className="text">
+                    {props.text}
+                </div>
+            </div>
+        </div>
+    );
+}
+export default CommentDetail;
